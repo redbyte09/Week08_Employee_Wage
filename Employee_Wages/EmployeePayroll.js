f8bf44170015d96a36e00b915f2fad74ab44634c
redbyte09 <sakshisharma4234@gmail.com>
2025-03-04 16:48:42 +0530

    [Sakshi] Add .Extended Employee Payroll Data with Gender and Start Date

--- Employee_Wages/EmployeePayroll.js
+++ Employee_Wages/EmployeePayroll.js
@@ -1,15 +1,20 @@
+
 class EmployeePayroll {
-    constructor(id, name, salary) {
+    constructor(id, name, salary, gender, startDate) {
         this.id = id;
         this.name = name;
         this.salary = salary;
+        this.gender = gender;
+        this.startDate = new Date(startDate);
     }
 
-    getDetails = () => `ID: ${this.id}, Name: ${this.name}, Salary: $${this.salary}`;
+   
+    getDetails = () => 
+        `ID: ${this.id}, Name: ${this.name}, Salary: $${this.salary}, Gender: ${this.gender}, Start Date: ${this.startDate.toDateString()}`;
 }
 
-const emp1 = new EmployeePayroll(101, "Anand Soni", 5000);
-const emp2 = new EmployeePayroll(102, "Sakshi Sharma", 7000);
+const emp1 = new EmployeePayroll(101, "Anand Soni", 5000, "Male", "2023-05-10");
+const emp2 = new EmployeePayroll(102, "Ankit Kumar", 7000, "Female", "2022-11-15");
 
 console.log(emp1.getDetails());
 console.log(emp2.getDetails());
